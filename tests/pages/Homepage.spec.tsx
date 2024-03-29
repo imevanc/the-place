d0e72906ed47dd5f8be0538be825e4e47627e6c1
munaciella <franes88@gmail.com>
2024-03-29 23:50:02 +0000

    add navbar components and tests to homepage

--- tests/pages/Homepage.spec.tsx
+++ tests/pages/Homepage.spec.tsx
@@ -1,6 +1,7 @@
 import { screen } from "@testing-library/react";
 import { Homepage } from "@/app";
 import { renderPage } from "../utils";
+import { Navbar } from "@/components";
 
 test("renders heading", () => {
   renderPage(<Homepage />);
@@ -8,4 +9,22 @@ test("renders heading", () => {
     name: /Hello World!/i,
   });
   expect(heading).toBeVisible();
+});
+
+test("renders navbar", () => {
+  renderPage(<Homepage />);
+  const navbar: HTMLElement = screen.getAllByRole("navigation")[0];
+  expect(navbar).toBeVisible();
+  const about: HTMLElement = screen.getAllByRole("link", {
+    name: /About/i,
+  })[0];
+  expect(about).toBeVisible();
+  const explorebycity: HTMLElement = screen.getAllByRole("link", {
+    name: /Explore by city/i,
+  })[0];
+  expect(explorebycity).toBeVisible();
+  const signin: HTMLElement = screen.getAllByRole("button", {
+    name: /Sign In/i,
+  })[0];
+  expect(signin).toBeVisible();
 });
