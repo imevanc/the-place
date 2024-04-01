cc0efc2ea7eabe91acf0f487ad3077156537a751
munaciella <franes88@gmail.com>
2024-04-01 23:39:35 +0100

    rename components

--- tests/pages/Homepage.spec.tsx
+++ tests/pages/Homepage.spec.tsx
@@ -2,7 +2,6 @@ import { screen } from "@testing-library/react";
 import { Homepage } from "@/app";
 import { renderPage } from "../utils";
 
-
 test("renders heading", () => {
   renderPage(<Homepage />);
   const heading: HTMLElement = screen.getByRole("heading", {
