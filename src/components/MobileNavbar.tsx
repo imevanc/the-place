f028a5f32559648738ca0aebda6eab4778153feb
munaciella <franes88@gmail.com>
2024-04-07 23:47:40 +0100

    add mobile navbar

--- src/components/MobileNavbar.tsx
+++ src/components/MobileNavbar.tsx
@@ -1,8 +1,7 @@
-"use client";
+'use client';
 import React, { useState } from 'react';
-import Link from 'next/link';
 import { HiMenuAlt3, HiX } from 'react-icons/hi';
-import { FC } from "react";
+import { FC } from 'react';
 
 export const MobileNavbar: FC = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -13,36 +12,46 @@ export const MobileNavbar: FC = () => {
 
   return (
     <nav className="bg-blue-500 text-white p-4 flex justify-between items-center">
-      <div className="flex items-center">
-        <div className="text-2xl font-bold mr-4">Logo Placeholder</div>
+      <ol className="flex items-center">
+        <li className="text-2xl font-bold mr-4">Logo Placeholder</li>
         <button onClick={toggleMenu} className="lg:hidden">
           {isMenuOpen ? <HiX size={24} /> : <HiMenuAlt3 size={24} />}
         </button>
-      </div>
-      <div className={`lg:flex lg:items-center ${isMenuOpen ? 'block' : 'hidden'}`}>
-        {/* <Link href="/">
-          <a className="block mt-4 lg:inline-block lg:mt-0 text-white mr-4">
+
+        <div
+          className={`lg:flex lg:items-center ${isMenuOpen ? 'block' : 'hidden'}`}
+        ></div>
+        <li className="mr-4">
+          <a href="/" className="block mt-4 lg:inline-block lg:mt-0 text-white">
             Explore by City
           </a>
-        </Link>
-        <Link href="/about">
-          <a className="block mt-4 lg:inline-block lg:mt-0 text-white mr-4">
+        </li>
+
+        <li>
+          <a
+            href="/about"
+            className="block mt-4 lg:inline-block lg:mt-0 text-white mr-4"
+          >
             About
           </a>
-        </Link>
-        <Link href="/signin">
-          <a className="block mt-4 lg:inline-block lg:mt-0 text-white mr-4">
-            Sign In
-          </a>
-        </Link> */}
-        <div className="block mt-4 lg:inline-block lg:mt-0">
-          <a href="#" className="text-white mr-4">
-            Download the App
-          </a>
-          <button onClick={toggleMenu} className="lg:hidden">
-            <HiX size={24} />
-          </button>
-        </div>
+        </li>
+      </ol>
+      <button
+        onClick={() => console.log('Sign In clicked')}
+        className="block mt-4 lg:inline-block lg:mt-0 text-white mr-4"
+      >
+        Sign In
+      </button>
+      <div className="block mt-4 lg:inline-block lg:mt-0">
+        <button
+          onClick={() => console.log('Sign In clicked')}
+          className="text-white mr-4"
+        >
+          Download the App
+        </button>
+        <button onClick={toggleMenu} className="lg:hidden">
+          <HiX size={24} />
+        </button>
       </div>
     </nav>
   );
