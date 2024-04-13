c25f8a9b9da401fb963ffa486fd37cd685ce34ac
munaciella <franes88@gmail.com>
2024-04-14 00:44:21 +0100

    fix the mobile navbar layout

--- src/components/MobileNavbar.tsx
+++ src/components/MobileNavbar.tsx
@@ -1,7 +1,7 @@
-"use client";
-import React, { useState, Fragment } from "react";
-import { HiMenuAlt3, HiX } from "react-icons/hi";
-import { FC } from "react";
+'use client';
+import React, { useState, Fragment } from 'react';
+import { HiMenuAlt3, HiX } from 'react-icons/hi';
+import { FC } from 'react';
 
 export const MobileNavbar: FC = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -11,19 +11,16 @@ export const MobileNavbar: FC = () => {
   };
 
   return (
-    <nav className="bg-blue-500 text-white p-4 flex sm:hidden justify-between items-center">
-      <ol className="flex items-center">
-        <li className="text-2xl font-bold mr-4">Logo Placeholder</li>
-        <button onClick={toggleMenu} className="lg:hidden">
-          {isMenuOpen ? <HiX size={24} /> : <HiMenuAlt3 size={24} />}
-        </button>
+    <nav className="text-primary-500 p-4 flex sm:hidden justify-between items-center">
+      <ol className="flex items-center justify-items-center">
+        <li className="text-2xl font-bold mr-6">Logo Placeholder</li>
 
         {isMenuOpen && (
           <Fragment>
             <li className="mr-4">
               <a
                 href="/"
-                className="block mt-4 lg:inline-block lg:mt-0 text-white"
+                className="block mt-4 lg:inline-block lg:mt-0 text-primary-400"
               >
                 Explore by City
               </a>
@@ -32,31 +29,33 @@ export const MobileNavbar: FC = () => {
             <li>
               <a
                 href="/about"
-                className="block mt-4 lg:inline-block lg:mt-0 text-white mr-4"
+                className="block mt-2 lg:inline-block lg:mt-0 text-primary-400 mr-4"
               >
                 About
               </a>
             </li>
+
+            <button
+              onClick={() => console.log('Sign In clicked')}
+              className="block mt-4 lg:inline-block lg:mt-0 text-primary-400 mr-4"
+            >
+              Sign In
+            </button>
+            <button
+              onClick={() => console.log('Sign In clicked')}
+              className="text-primary-400 mr-4 mt-4"
+            >
+              Download the App
+            </button>
           </Fragment>
         )}
       </ol>
-      <button
-        onClick={() => console.log("Sign In clicked")}
-        className="block mt-4 lg:inline-block lg:mt-0 text-white mr-4"
-      >
-        Sign In
+      <button onClick={toggleMenu} className="lg:hidden">
+        {isMenuOpen ? <HiX size={24} /> : <HiMenuAlt3 size={24} />}
       </button>
-      <div className="block mt-4 lg:inline-block lg:mt-0">
-        <button
-          onClick={() => console.log("Sign In clicked")}
-          className="text-white mr-4"
-        >
-          Download the App
-        </button>
-        <button onClick={toggleMenu} className="lg:hidden">
-          <HiX size={24} />
-        </button>
-      </div>
+      {/* <button onClick={toggleMenu} className="lg:hidden">
+        <HiX size={24} />
+      </button> */}
     </nav>
   );
 };
