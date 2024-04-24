8753e0c5aa836080b86a1eda0bff443d659be320
munaciella <franes88@gmail.com>
2024-04-24 14:08:27 +0100

    add bg primary

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,15 +12,15 @@ const footerLinks: Array<{ href: string; icon: ReactElement }> = [
   { href: 'https://www.x.com', icon: <FaXTwitter /> },
 ];
 
-const { nav, footer } = copy.common;
+const { footer } = copy.common;
 
 export const Footer: FC<{
   companyName: string;
 }> = ({ companyName }) => {
   return (
-    <footer className="w-full bg-white relative bottom-0 py-4 px-4">
+    <footer className="w-full bg-primary-300 relative bottom-0 py-4 px-4">
       <div className="w-full flex flex-col md:flex-row justify-between items-center">
-        <span className="text-primary-400">
+        <span className="text-secondary-900">
           &copy; {new Date().getFullYear()} {companyName} {footer.msg}
         </span>
         <div className="md:w-[10%] flex justify-between items-center mb-4 md:mb-0 order-first md:order-last">
@@ -30,7 +30,7 @@ export const Footer: FC<{
               href={href}
               target="_blank"
               rel="noopener noreferrer"
-              className="inline-block mr-3 text-2xl text-secondary-800 hover:text-primary-600"
+              className="inline-block mr-3 text-2xl text-secondary-800 hover:text-primary-900"
             >
               {icon}
             </Link>
