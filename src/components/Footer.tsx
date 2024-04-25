721f940812b6fcad1ee64c5e8c6ac63da27d00fc
munaciella <franes88@gmail.com>
2024-04-25 14:59:37 +0100

    add a span

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,46 @@
+// /* eslint-disable @next/next/no-img-element */
+// 'use client';
+// import { FC, ReactElement } from 'react';
+// import Link from 'next/link';
+// import { FaFacebook, FaInstagram } from 'react-icons/fa';
+// import { FaXTwitter } from 'react-icons/fa6';
+// import { copy } from '@/copy';
+
+// const footerLinks: Array<{ href: string; icon: ReactElement }> = [
+//   { href: 'https://www.facebook.com', icon: <FaFacebook /> },
+//   { href: 'https://www.instagram.com', icon: <FaInstagram /> },
+//   { href: 'https://www.x.com', icon: <FaXTwitter /> },
+// ];
+
+// const { footer } = copy.common;
+
+// export const Footer: FC<{
+//   companyName: string;
+// }> = ({ companyName }) => {
+//   return (
+//     <footer className="w-full bg-primary-300 relative bottom-0 py-4 px-4">
+//       <div className="w-full flex flex-col md:flex-row justify-between items-center">
+//         <span className="text-secondary-900">
+//           &copy; {new Date().getFullYear()} {companyName} {footer.msg}
+//         </span>
+//         <div className="md:w-[10%] flex justify-between items-center mb-4 md:mb-0 order-first md:order-last">
+//           {footerLinks.map(({ icon, href }, idx) => (
+//             <Link
+//               key={href + idx}
+//               href={href}
+//               target="_blank"
+//               rel="noopener noreferrer"
+//               className="inline-block mr-3 text-2xl text-secondary-800 hover:text-primary-900"
+//             >
+//               {icon}
+//             </Link>
+//           ))}
+//         </div>
+//       </div>
+//     </footer>
+//   );
+// };
+
 /* eslint-disable @next/next/no-img-element */
 'use client';
 import { FC, ReactElement } from 'react';
@@ -6,10 +49,10 @@ import { FaFacebook, FaInstagram } from 'react-icons/fa';
 import { FaXTwitter } from 'react-icons/fa6';
 import { copy } from '@/copy';
 
-const footerLinks: Array<{ href: string; icon: ReactElement }> = [
-  { href: 'https://www.facebook.com', icon: <FaFacebook /> },
-  { href: 'https://www.instagram.com', icon: <FaInstagram /> },
-  { href: 'https://www.x.com', icon: <FaXTwitter /> },
+const footerLinks: Array<{ href: string; icon: ReactElement; name: string }> = [
+  { href: 'https://www.facebook.com', icon: <FaFacebook />, name: 'Facebook' },
+  { href: 'https://www.instagram.com', icon: <FaInstagram />, name: 'Instagram' },
+  { href: 'https://www.x.com', icon: <FaXTwitter />, name: 'Twitter' },
 ];
 
 const { footer } = copy.common;
@@ -24,16 +67,18 @@ export const Footer: FC<{
           &copy; {new Date().getFullYear()} {companyName} {footer.msg}
         </span>
         <div className="md:w-[10%] flex justify-between items-center mb-4 md:mb-0 order-first md:order-last">
-          {footerLinks.map(({ icon, href }, idx) => (
-            <Link
-              key={href + idx}
-              href={href}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="inline-block mr-3 text-2xl text-secondary-800 hover:text-primary-900"
-            >
-              {icon}
-            </Link>
+          {footerLinks.map(({ icon, href, name }, idx) => (
+            <div key={href + idx} >
+              <Link
+                href={href}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="inline-block mr-3 text-2xl text-secondary-800 hover:text-primary-900"
+              >
+                {icon}
+              </Link>
+              <span className="sr-only">{name}</span>
+            </div>
           ))}
         </div>
       </div>
